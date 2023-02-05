d71a79a3d17158df7d24355dbe192e47525754b0
Ahsan <ahsansatti402@gmail.com>
2023-02-05 16:34:50 +0500

    complete using tawlind css, redux, google map api

---
+++ App.js
@@ -0,0 +1,40 @@
+import {KeyboardAvoidingView ,Platform} from 'react-native';
+import { Provider } from 'react-redux';
+import HomeScreen from './screens/HomeScreen';
+import MapScreen from './screens/MapScreen';
+import { store } from './store';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+export default function App() {
+  const Stack = createNativeStackNavigator();
+  return (
+    <Provider store={store}>
+    <NavigationContainer>
+    <SafeAreaProvider>
+<KeyboardAvoidingView 
+behavior={Platform.OS === "ios" ? "padding" : "height"}
+style={{flex: 1}}
+keyboardVerticalOffset={Platform.OS === "ios" ? -64 : 0}
+>
+    <Stack.Navigator>
+        <Stack.Screen 
+        name="HomeScreen" 
+        component={HomeScreen} 
+        options={{headerShown: false,}} 
+        />
+         <Stack.Screen 
+        name="MapScreen" 
+        component={MapScreen} 
+        options={{headerShown: false,}} 
+        />
+      </Stack.Navigator>
+      </KeyboardAvoidingView>
+    </SafeAreaProvider>
+    </NavigationContainer>
+
+    </Provider>
+  );
+}
+
+
---
+++ screens/HomeScreen.js
@@ -0,0 +1,69 @@
+import { StyleSheet, Text, View,SafeAreaView, Image} from 'react-native'
+import React from 'react'
+import tw from 'tailwind-react-native-classnames'
+import NavOptions from '../components/NavOptions'
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
+import {GOOGLE_MAPS_APIKEY} from "@env";
+import { useDispatch } from 'react-redux';
+import { setDestination, setOrigin } from '../slices/navSlice';
+import NavFavourites from '../components/NavFavourites';
+const HomeScreen = () => {
+const dispatch = useDispatch();
+  return (
+    <SafeAreaView style={tw `bg-white h-full`}>
+{/* combine tawlind and external css */}
+
+      {/* <Text style={[tw`text-red-500 p-10`, styles.text]}>I am HomeScreen</Text> */}
+
+{/* combine tawlind and inline css */}
+
+          {/* <Text style={[tw`text-red-500 p-10`, {
+            color: "green",
+          }]}>I am HomeScreen</Text> */} 
+    <View style={tw`p-5`}>
+        <Image
+        style={{width: 100, height: 100, resizeMode: "contain"}}
+         source={{
+            uri: "https://links.papareact.com/gzs"
+        }}/>
+        <GooglePlacesAutocomplete
+        placeholder='where from' 
+        styles={{
+            container: {
+                flex: 0,
+            },
+            textInput:{
+                fontSize: 18,
+            },
+        }}
+        onPress={(data, details= null)=>{
+dispatch(setOrigin({
+    location: details.geometry.location,
+    description: data.description
+}));
+dispatch(setDestination(null))
+        }}
+        fetchDetails={true}
+        returnKeyType={"search"}
+        enablePoweredByContainer={false}
+        minLength={2}
+        query={{
+            key: GOOGLE_MAPS_APIKEY,
+            language: 'en'
+        }}
+            nearbyPlacesAPI='GooglePlacesSearch'
+            debounce={400}
+        />
+    <NavOptions/>
+    <NavFavourites />
+    </View>
+    </SafeAreaView>
+    
+  )
+}
+export default HomeScreen
+const styles = StyleSheet.create({
+    text: {
+        fontSize: 48
+    }
+})
